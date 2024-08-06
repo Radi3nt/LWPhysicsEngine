8629dfe3dc7c7eb3c35a2b0bd3242755e7094841
Radi3nt <pro.radi3nt@gmail.com>
2024-08-06 11:36:30 +0200

    Added NoSleepingData.INSTANCE

--- physics-core/src/main/java/fr/radi3nt/physics/sleeping/NoSleepingData.java
+++ physics-core/src/main/java/fr/radi3nt/physics/sleeping/NoSleepingData.java
@@ -4,6 +4,12 @@ import fr.radi3nt.maths.components.vectors.Vector3f;
 import fr.radi3nt.physics.core.state.DynamicsData;
 
 public class NoSleepingData implements SleepingData {
+
+    public static final SleepingData INSTANCE = new NoSleepingData();
+
+    public NoSleepingData() {
+    }
+
     @Override
     public void ode(Vector3f force, Vector3f torque) {
 
