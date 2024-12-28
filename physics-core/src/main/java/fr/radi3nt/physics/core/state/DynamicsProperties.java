bf99e3c99b7aed5f11ac7c09a2b1e3013675862c
Radi3nt <pro.radi3nt@gmail.com>
2024-12-28 18:43:02 +0100

    Added copy, equals and toString to DynamicsProperties

--- physics-core/src/main/java/fr/radi3nt/physics/core/state/DynamicsProperties.java
+++ physics-core/src/main/java/fr/radi3nt/physics/core/state/DynamicsProperties.java
@@ -2,6 +2,8 @@ package fr.radi3nt.physics.core.state;
 
 import fr.radi3nt.maths.components.advanced.matrix.Matrix3x3;
 
+import java.util.Objects;
+
 public class DynamicsProperties {
 
     public float inverseMass;
@@ -18,5 +20,46 @@ public class DynamicsProperties {
         this.kineticFrictionFactor = kineticFrictionFactor;
         this.staticFrictionFactor = staticFrictionFactor;
         this.staticFrictionThreshold = staticFrictionThreshold;
+    }
+
+    public void copy(DynamicsProperties properties) {
+        this.inverseMass = properties.inverseMass;
+        this.bodyInverseInertiaTensor = properties.bodyInverseInertiaTensor;
+        this.bouncingFactor = properties.bouncingFactor;
+        this.kineticFrictionFactor = properties.kineticFrictionFactor;
+        this.staticFrictionFactor = properties.staticFrictionFactor;
+        this.staticFrictionThreshold = properties.staticFrictionThreshold;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DynamicsProperties)) return false;
+
+        DynamicsProperties that = (DynamicsProperties) o;
+        return Float.compare(inverseMass, that.inverseMass) == 0 && Float.compare(bouncingFactor, that.bouncingFactor) == 0 && Float.compare(kineticFrictionFactor, that.kineticFrictionFactor) == 0 && Float.compare(staticFrictionFactor, that.staticFrictionFactor) == 0 && Float.compare(staticFrictionThreshold, that.staticFrictionThreshold) == 0 && Objects.equals(bodyInverseInertiaTensor, that.bodyInverseInertiaTensor);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Float.hashCode(inverseMass);
+        result = 31 * result + Objects.hashCode(bodyInverseInertiaTensor);
+        result = 31 * result + Float.hashCode(bouncingFactor);
+        result = 31 * result + Float.hashCode(kineticFrictionFactor);
+        result = 31 * result + Float.hashCode(staticFrictionFactor);
+        result = 31 * result + Float.hashCode(staticFrictionThreshold);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "DynamicsProperties{" +
+                "inverseMass=" + inverseMass +
+                ", bodyInverseInertiaTensor=" + bodyInverseInertiaTensor +
+                ", bouncingFactor=" + bouncingFactor +
+                ", kineticFrictionFactor=" + kineticFrictionFactor +
+                ", staticFrictionFactor=" + staticFrictionFactor +
+                ", staticFrictionThreshold=" + staticFrictionThreshold +
+                '}';
     }
 }
