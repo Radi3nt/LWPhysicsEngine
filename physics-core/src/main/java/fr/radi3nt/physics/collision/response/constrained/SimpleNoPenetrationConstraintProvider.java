5e488239961aedd49ea6d8d2c1df84ca434b859d
Radi3nt <pro.radi3nt@gmail.com>
2024-12-29 00:57:03 +0100

    Restricted resorting to simultaneous solver

--- physics-core/src/main/java/fr/radi3nt/physics/collision/response/constrained/SimpleNoPenetrationConstraintProvider.java
+++ physics-core/src/main/java/fr/radi3nt/physics/collision/response/constrained/SimpleNoPenetrationConstraintProvider.java
@@ -4,6 +4,7 @@ import fr.radi3nt.maths.components.vectors.Vector3f;
 import fr.radi3nt.maths.components.vectors.implementations.SimpleVector3f;
 import fr.radi3nt.physics.collision.contact.manifold.PersistentManifold;
 import fr.radi3nt.physics.collision.contact.manifold.contact.ContactPoint;
+import fr.radi3nt.physics.collision.response.sequential.SequentialImpulseCollisionContactSolver;
 import fr.radi3nt.physics.constraints.constraint.Constraint;
 import fr.radi3nt.physics.constraints.constraint.DriftParameters;
 import fr.radi3nt.physics.constraints.constraint.caching.provider.ArrayCachingModuleProvider;
@@ -14,15 +15,9 @@ import fr.radi3nt.physics.constraints.constraint.index.RigidBodyIndex;
 
 import java.util.Collection;
 
-import static java.lang.Math.abs;
-
 public class SimpleNoPenetrationConstraintProvider implements NoPenetrationConstraintProvider {
 
-    private static final Vector3f[] AXIS = new Vector3f[] {
-            new SimpleVector3f(1, 0, 0),
-            new SimpleVector3f(0, 1, 0),
-            new SimpleVector3f(0, 0, 1),
-    };
+    private static final float VELOCITY_THRESHOLD = 1E-1f;
     private final DriftParameters driftParameters;
 
     public SimpleNoPenetrationConstraintProvider(DriftParameters driftParameters) {
@@ -31,30 +26,29 @@ public class SimpleNoPenetrationConstraintProvider implements NoPenetrationConst
 
     @Override
     public void addConstraint(Collection<Constraint> constraints, PersistentManifold persistentManifold, ContactPoint contactPoint) {
+        if (!contactPoint.isRestingContact())
+            return;
+
         RigidBodyIndex indexA = new ImplicitRigidBodyIndex(persistentManifold.getObjectA());
         RigidBodyIndex indexB = new ImplicitRigidBodyIndex(persistentManifold.getObjectB());
         constraints.add(new NoPenetrationConstraint(driftParameters, contactPoint.normal, contactPoint.rA, contactPoint.rB, indexA, indexB, new ArrayCachingModuleProvider(new NoPenetrationCachingConstraintModule(contactPoint.manifoldPoint.data))));
 
-        Vector3f randomVec = chooseAxis(contactPoint.normal);
-        if (randomVec==null)
-            return;
-        Vector3f tangent = contactPoint.normal.duplicate().cross(randomVec).normalize();
-        Vector3f otherTangent = contactPoint.normal.duplicate().cross(tangent);
-        float uk = contactPoint.a.getBodyProperties().kineticFrictionFactor*contactPoint.b.getBodyProperties().kineticFrictionFactor;
-        float relativeVel = contactPoint.manifoldPoint.data.cachedContactLambda;
-        if (relativeVel!=0 && uk!=0)
-            constraints.add(new FrictionConstraint(tangent, otherTangent, relativeVel, uk, contactPoint.rA, contactPoint.rB, indexA, indexB, new ArrayCachingModuleProvider(new FrictionCachingConstraintModule(contactPoint.manifoldPoint.data, 0), new FrictionCachingConstraintModule(contactPoint.manifoldPoint.data, 1))));
-    }
+        Vector3f tangent = new SimpleVector3f();
+        Vector3f otherTangent = new SimpleVector3f();
+        SequentialImpulseCollisionContactSolver.computeTangents(tangent, otherTangent, contactPoint.normal, contactPoint.getRelativeVelocityVec());
 
-    private Vector3f chooseAxis(Vector3f contactNormal) {
-        Vector3f random = AXIS[0];
-        int i = 1;
-        while (abs(random.dot(contactNormal))>=0.99f) {
-            if (i>=AXIS.length)
-                return null;
-            random = AXIS[i];
-            i++;
-        }
-        return random;
+        float uk = contactPoint.a.getBodyProperties().kineticFrictionFactor * contactPoint.b.getBodyProperties().kineticFrictionFactor;
+        float relativeVel = contactPoint.manifoldPoint.data.cachedContactLambda;
+        if (relativeVel*uk > VELOCITY_THRESHOLD)
+            constraints.add(new FrictionConstraint(tangent,
+                    otherTangent,
+                    relativeVel,
+                    uk,
+                    contactPoint.rA,
+                    contactPoint.rB,
+                    indexA,
+                    indexB,
+                    new ArrayCachingModuleProvider(new FrictionCachingConstraintModule(contactPoint.manifoldPoint.data, 0),
+                            new FrictionCachingConstraintModule(contactPoint.manifoldPoint.data, 1))));
     }
 }
